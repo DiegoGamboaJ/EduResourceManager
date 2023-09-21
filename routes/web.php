9ddf985e500921f3ae7ea07b7b405ef3bda8f255
Diego Gamboa <diego.gamboaj29@gmail.com>
2023-09-20 23:50:10 -0300

    modificacion en rutas web

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post('reservation/save', [ReservationController::class, 'save'])->name('reservation.save');
+    Route::post('reservation/save', [ReservationController::class, 'save'])->name('reservation.save');
 });
 
 Route::middleware('auth', 'verified')->group(function () {
